091d49585e799f953b0e3a327855c584b8d7a475
sergeikabuldzhanov <thesiegebot@gmail.com>
2019-11-05 21:03:26 +0500

    Started on functionality

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
   const [displayValue, setDisplayValue] = React.useState(0);
+  const [sequence, setSequence] = React.useState([0]);
 
   return (
     <div className="container">
